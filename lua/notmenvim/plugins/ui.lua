b45865c759ef84efc1c50c3b94d22e14cbc0993e
amooozk <shantulipune1@gmail.com>
2024-07-24 20:38:59 +0530

    configured starter 3

--- lua/notmenvim/plugins/ui.lua
+++ lua/notmenvim/plugins/ui.lua
@@ -415,11 +415,13 @@ return {
       vim.api.nvim_create_autocmd("User", {
         pattern = "NotMeNvimStarted",
         callback = function(ev)
-          local pad_footer = string.rep(" ", 10)
-          starter.config.footer = " Moik Moik "
-          -- if vim.bo[ev.buf].filetype == "ministarter" then
-          --   pcall(starter.refresh)
-          -- end
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          local pad_footer = string.rep(" ", 8)
+          starter.config.footer = pad_footer .. "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+          if vim.bo[ev.buf].filetype == "ministarter" then
+            pcall(starter.refresh)
+          end
         end,
       })
     end,
