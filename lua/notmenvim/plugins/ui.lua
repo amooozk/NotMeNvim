a411d5debc5e5aa66072b4255020b968b1280a0b
amooozk <shantulipune1@gmail.com>
2024-07-25 15:25:15 +0530

    configured noice

--- lua/notmenvim/plugins/ui.lua
+++ lua/notmenvim/plugins/ui.lua
@@ -14,7 +14,7 @@ return {
     },
     opts = {
       stages = "static",
-      timeout = 3000,
+      timeout = 1000,
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
       end,
